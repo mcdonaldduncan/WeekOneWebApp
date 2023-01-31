91c03c9651aafa54b1484cf0c18222c109098078
mcdonaldduncan <380939@colum.edu>
2023-01-30 19:49:02 -0600

    update

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,6 +13,16 @@ namespace WeekOneWebApp.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Assign ViewBag values and return view
+        /// </summary>
+        /// <param name="_yearOfBirth"></param>
+        /// <param name="_quote"></param>
+        /// <param name="_firstName"></param>
+        /// <param name="_lastName"></param>
+        /// <param name="_age"></param>
+        /// <param name="_occupation"></param>
+        /// <returns></returns>
         public IActionResult Result(int _yearOfBirth, string _quote, string _firstName, string _lastName, int _age, string _occupation)
         {
             ViewBag.YearOfBirth = _yearOfBirth;
@@ -25,6 +35,11 @@ namespace WeekOneWebApp.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Take form input and calculate aproximate year before redirecting to Result with necessary values
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
         // POST: UserController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -33,6 +48,7 @@ namespace WeekOneWebApp.Controllers
             try
             {
                 int yearOfBirth = -1;
+                // TryParse is not really necessary here because the form allows number entry only but I left it in as best practice
                 if (Int32.TryParse(collection["Age"], out int age))
                 {
                     yearOfBirth = DateTime.Now.Year - age;
